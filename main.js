49158b25ecb8404dde9c87db6e6221bef4361fa9
Win Feigle <win.feigle@gmail.com>
2022-06-08 20:43:39 -0600

    commit tree 75a46b1196f8f894247bf30092723839131639dc

--- main.js
+++ main.js
@@ -3,7 +3,44 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('#modal')
+  
+const likeButton = document.querySelectorAll('.like-glyph')
 
+const like = e => {
+  const heart = e.target
+  mimicServerCall('bogusurl')
+  .then(() => {
+    if(heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.className = 'activated-heart'
+    } else{
+      heart.innerText = EMPTY_HEART
+      heart.className = ''
+    }
+  })
+  .catch((error) => {
+    modal.className = ''
+    modal.innerHTML = error
+    setTimeout(() => modal.className = 'hidden', 3000)
+  })
+}
+
+for(const i of likeButton){
+  i.addEventListener('click', like)
+}
+
+// likeButton.addEventListener('click', 
+//   mimicServerCall()
+//   .then(() => {
+//     likeButton.textContent = FULL_HEART
+//     likeButton.className = 'activated-heart'
+//   })
+//   .catch((error) => {
+//     modal.className = ''
+//     modal.innerHTML = error
+//   })
+// )
 
 
 
